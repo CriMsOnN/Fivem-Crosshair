b24c833ccbdf2872faaa1258dacdfac2ba8c94c6
Cr1MsOn <tasosmoustakis1995@gmail.com>
2020-12-04 20:52:03 +0200

    :sparkles: Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+    'shared/config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    'shared/config.lua',
+    'server/main.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+    'html/css/style.css',
+    'html/js/app.js',
+}
